2ac30022a3e4972c3fb99bd466bc7b7f4df4d4a6
George Gordon <ggordon2019@fau.edu>
2023-09-21 21:54:51 -0400

    Initial Commit

---
+++ All About Me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  All About Me
+//
+//  Created by George Gordon on 9/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
