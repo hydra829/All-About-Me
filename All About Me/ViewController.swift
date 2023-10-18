108c11cda3f580ab92b4011fded28e0b7957bf03
George Gordon <ggordon2019@fau.edu>
2023-10-17 21:21:14 -0400

    fixed constraints and added tap gesture

--- All About Me/ViewController.swift
+++ All About Me/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTapButton(_ sender: UITapGestureRecognizer) {
+        
+        if let tappedView = sender.view {
+            performSegue(withIdentifier: "detailSegue", sender: tappedView)
+        }
+    }
+    
 }
-
